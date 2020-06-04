ebf272dd8248d31bdf7eb14876f2cf2880fd2623
panjingping <panjingping@gmail.com>
2020-06-04 13:41:41 +0800

    update index.py

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -6,11 +6,11 @@ class TestHandlerCase(unittest.TestCase):
 
     def test_response(self):
         print("testing response.")
-        result = index.handler(None, None)
+        result = index.lambda_handler(None, None)
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
-    
+
 
 if __name__ == '__main__':
     unittest.main()
