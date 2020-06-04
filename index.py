416069d9b5cf023b12e5b63af3364440919b1172
panjingping <panjingping@gmail.com>
2020-06-04 13:08:35 +0800

    update index.py dd

--- index.py
+++ index.py
@@ -1,12 +1,28 @@
-import json
-import datetime
+import uuid
+import boto3
+import os
 
 
-def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+# create_by panjingping
+# description: create short url token
+# create 2020.6.2
+
+def lambda_handler(event, context):
+    token = hex(uuid.getnode())
+
+    url = event['postData']['url']
+    customDomain = event['postData']['customDomain']
+
+    bucket = event['postData']['bucket']
+
+    short_url = customDomain + "/" + token
+    s3 = boto3.client('s3')
+    # TODO implement
+    try:
+        s3.put_object(ACL='public-read', Bucket=bucket, Key=token, WebsiteRedirectLocation=url, ContentType='text/html')
+
+        return {'statusCode': 200,
+                'body': json.dumps({'short_url': short_url, 'url': url, 'token': token})}
+    except Exception as e:
+        print(e)
+        raise e
